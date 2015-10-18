408b787af07f94acb8c0815d03c030dae3b248f0
Devin Brown <devin.g.brown@gmail.com>
2015-10-18 14:19:49 -0600

    eppic fail Jiang

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
   println!("Guess the number!");
+
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+
+  println!("The secrete number is: {}", secret_number);
+
+  println!("Please input your guess.");
 
   // The ::new() syntax uses :: because this is an ‘associated function’ of a
   // particular type. That is to say, it’s associated with String itself, rather
@@ -13,5 +23,15 @@ fn main() {
     .ok()
     .expect("Failed to read line");
 
-  print!("You guessed: {}", guess);
+  let guess: u32 = guess.trim().parse()
+    .ok()
+    .expect("Please type a number!");
+
+  println!("You guessed: {}", guess);
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less    => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal   => println!("You win!"),
+  }
 }
