5988ea1ce179fe391243c96a2b732569c69b78df
Devin Brown <devin.g.brown@gmail.com>
2015-10-18 14:32:12 -0600

    better ignores

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,33 @@ fn main() {
 
   let secret_number = rand::thread_rng().gen_range(1, 101);
 
-  println!("The secrete number is: {}", secret_number);
+  loop {
+    println!("Please input your guess.");
 
-  println!("Please input your guess.");
+    // The ::new() syntax uses :: because this is an ‘associated function’ of a
+    // particular type. That is to say, it’s associated with String itself, rather
+    // than a particular instance of a String. Some languages call this a ‘static
+    // method’.
+    let mut guess = String::new();
 
-  // The ::new() syntax uses :: because this is an ‘associated function’ of a
-  // particular type. That is to say, it’s associated with String itself, rather
-  // than a particular instance of a String. Some languages call this a ‘static
-  // method’.
-  let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+      .ok()
+      .expect("Failed to read line");
 
-  io::stdin().read_line(&mut guess)
-    .ok()
-    .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
-  let guess: u32 = guess.trim().parse()
-    .ok()
-    .expect("Please type a number!");
+    println!("You guessed: {}", guess);
 
-  println!("You guessed: {}", guess);
-
-  match guess.cmp(&secret_number) {
-    Ordering::Less    => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal   => println!("You win!"),
+    match guess.cmp(&secret_number) {
+      Ordering::Less    => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal   => {
+        println!("You win!");
+        break;
+      }
+    }
   }
 }
